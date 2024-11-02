8a7d91342b24d44bd057378c8ad8eb717b5c33ef
Micaele <micaellyvitoriac@gmail.com>
2024-11-02 15:40:03 -0300

    Adicionando nave do jogador

--- tela_principal.py
+++ tela_principal.py
@@ -1,23 +1,54 @@
+# ----- Importa e inicia pacotes
 import pygame
 import random
 
 pygame.init()
 
 # ----- Gera tela principal
-WIDTH = 800
-HEIGHT = 800
+WIDTH = 850
+HEIGHT = 750
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Ameaça Interestelar')
 
 # ----- Inicia assets
 ALIEN_WIDTH = 100
 ALIEN_HEIGHT = 76
+SHIP_WIDTH = 110  
+SHIP_HEIGHT = 90  
 font = pygame.font.SysFont(None, 48)
+
+#Adiciona fundo.
 background = pygame.image.load('imagens/SpaceBackGround.jpg').convert()
+#Adiciona imagem do Alien (invasor).
 alien_img = pygame.image.load('imagens/alien.png').convert_alpha()
 alien_img = pygame.transform.scale(alien_img, (ALIEN_WIDTH, ALIEN_HEIGHT))
+#Adiciona imagem da nave (jogador)
+ship_img = pygame.image.load('imagens/ship.png').convert_alpha()
+ship_img = pygame.transform.scale(ship_img, (SHIP_WIDTH, SHIP_HEIGHT))
 
 # ----- Inicia estruturas de dados
+# Definindo os novos tipos de classes.
+class Ship(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        # Atualização da posição da nave
+        self.rect.x += self.speedx
+
+        # Mantem a nave dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
 class Alien(pygame.sprite.Sprite):
     def __init__(self, img):
         # Construtor da classe mãe (Sprite).
@@ -34,7 +65,7 @@ class Alien(pygame.sprite.Sprite):
         # Atualizando a posição do alien
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        # Se o meteoro passar do final da tela, volta para cima e sorteia
+        # Se o alien passar do final da tela, volta para cima e sorteia
         # novas posições e velocidades
         if self.rect.top > HEIGHT or self.rect.right < 0 or self.rect.left > WIDTH:
             self.rect.x = random.randint(0, WIDTH-ALIEN_WIDTH)
@@ -47,12 +78,17 @@ game = True
 clock = pygame.time.Clock()
 FPS = 30
 
-# Criando um grupo de meteoros
-all_aliens = pygame.sprite.Group()
+# Criando um grupo de aliens
+all_sprites = pygame.sprite.Group()
+
+# Criando o jogador
+player = Ship(ship_img)
+all_sprites.add(player)
+
 # Criando os aliens
 for i in range(4):
-    meteor = Alien(alien_img)
-    all_aliens.add(meteor)
+    alien = Alien(alien_img)
+    all_sprites.add(alien)
 
 # ===== Loop principal =====
 while game:
@@ -66,14 +102,14 @@ while game:
 
     # ----- Atualiza estado do jogo
     # Atualizando a posição do alien
-    all_aliens.update()
+    all_sprites.update()
 
     # ----- Gera saídas
     window.fill((0, 0, 0))  # Preenche com a cor preta
     window.blit(background, (0, 0))
 
-    # Desenhando meteoros
-    all_aliens.draw(window)
+    # Desenhando aliens
+    all_sprites.draw(window)
 
     pygame.display.update()  # Mostra o novo frame para o jogador
 
